2dc3e81056d23b18d197dcb00258ba17dd5a8925
Jordon de Hoog <jordon.dehoog@gmail.com>
2015-05-23 17:02:07 -0400

    more logging changes

--- app/components/logger/logger-factory.js
+++ app/components/logger/logger-factory.js
@@ -28,29 +28,40 @@
 
     return service;
 
+    /**
+     * Public Methods
+     */
+
     function error(message, data, title) {
       toastr.error(message, title);
-      $log.error('Error: ' + message, data);
+      $log.error(getTimestamp() + '[Error] ' + message, data);
     }
 
     function info(message, data, title) {
       toastr.info(message, title);
-      $log.info('Info: ' + message, data);
+      $log.info(getTimestamp() + '[Info] ' + message, data);
     }
 
     function success(message, data, title) {
       toastr.success(message, title);
-      $log.info('Success: ' + message, data);
+      $log.info(getTimestamp() + '[Success] ' + message, data);
     }
 
     function warning(message, data, title) {
       toastr.warning(message, title);
-      $log.warn('Warning: ' + message, data);
+      $log.warn(getTimestamp() + '[Warning] ' + message, data);
     }
 
     function log(tag, message) {
-      var timestamp = '[' + $filter('date')(new Date(), 'yyyy/dd/MM|HH:mm:ss') + ']';
-      $log.log(timestamp + '[' + tag + '] ' + message);
+      $log.log(getTimestamp() + '[' + tag + '] ' + message);
+    }
+
+    /**
+     * Private Methods
+     */
+
+    function getTimestamp() {
+      return '[' + $filter('date')(new Date(), 'yyyy/dd/MM HH:mm:ss') + ']';
     }
   }
 }());
